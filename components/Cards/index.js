5ad07794321b80c65b624f69ffadc02ba892296a
Ken Diehl <diehlkj.99@gmail.com>
2019-11-08 22:20:29 -0500

    Completed card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const newCard = (article) => {
+    const card = document.createElement('div');
+        const headline = document.createElement('div');
+        const authorContainer = document.createElement('div');
+            const imgContainer = document.createElement('div');
+                const img = document.createElement('img');
+            const author = document.createElement('span');
+
+    headline.textContent = article.headline;
+    img.src= article.authorPhoto;
+    author.textContent = article.authorName;
+            
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authorContainer.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    card.appendChild(headline);
+    card.appendChild(authorContainer);
+        authorContainer.appendChild(imgContainer);
+            imgContainer.appendChild(img);
+        authorContainer.appendChild(author);
+
+    return card;
+}
+const cardContainer = document.querySelector('.cards-container');
+
+axios
+  .get('https://lambda-times-backend.herokuapp.com/articles')
+  .then((response) => {
+      console.log(response);
+      console.log(Object.values(response.data.articles));
+      Object.values(response.data.articles).forEach((topic) => {
+          topic.forEach((article) => {
+            console.log(article.authorName);
+            cardContainer.appendChild(newCard(article));
+          });
+      });
+  })
+  .catch((error) => {
+      console.log('Lambda Times Backend Error: ', error);
+  });
